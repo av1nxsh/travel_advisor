70074ae9d563a82259829d67eb1f0065e4aff199
AT <tumuavinash@gmail.com>
2022-06-04 02:08:51 +0530

    test commit fixing e-mail issue

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ const App = () =>  {
                         coordinates={coordinates}
                     />
                 </Grid>
-            </Grid>
+            </Grid> 
         </>
     );
 }
